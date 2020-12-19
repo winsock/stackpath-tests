c003dd9b3cb7032810ebc405e60d9330b0d1b9d8
Kevin Laude <nerfyoda@gmail.com>
2020-12-18 20:08:44 -0600

    Don't force STDIN to be a named pipe

--- input-processing/main.go
+++ input-processing/main.go
@@ -3,25 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"os"
 )
 
 func main() {
 	fmt.Println("SP// Backend Developer Test - Input Processing")
 	fmt.Println()
-
-	// Make sure input is being piped to this program. STDIN should be a
-	// named pipe.
-	info, err := os.Stdin.Stat()
-	if err != nil {
-		panic(err)
-	}
-
-	if info.Mode()&os.ModeNamedPipe != 0 {
-		fmt.Println("Please pipe input to this program.")
-		return
-	}
 
 	// Read STDIN into a new buffered reader
 	reader := bufio.NewReader(os.Stdin)
