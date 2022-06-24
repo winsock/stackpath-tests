356ba9ca769b0450fe0922582e07b506f827d2d6
Andrew Querol <andrew@querol.me>
2022-06-24 10:15:30 -0500

    Start of testing, fix the edge case where zero is passed in for fizz or buzz

--- unit-testing/pkg/fizzbuzz/fizzbuzz.go
+++ unit-testing/pkg/fizzbuzz/fizzbuzz.go
@@ -4,6 +4,11 @@ import (
 	"strconv"
 )
 
+const (
+	Fizz = "Fizz"
+	Buzz = "Buzz"
+)
+
 // FizzBuzz performs a FizzBuzz operation over a range of integers
 //
 // Given a range of integers:
@@ -11,25 +16,37 @@ import (
 // - Return "Buzz" if the integer is divisible by the `buzzAt` value.
 // - Return "FizzBuzz" if the integer is divisible by both the `fizzAt` and
 //   `buzzAt` values.
-// - Return the original number if is is not divisible by either the `fizzAt` or
+// - Return the original number if it is not divisible by either the `fizzAt` or
 //   the `buzzAt` values.
 func FizzBuzz(total, fizzAt, buzzAt int64) []string {
 	result := make([]string, total)
 
 	for i := int64(1); i <= total; i++ {
-		if !(i%fizzAt == 0) && !(i%buzzAt == 0) {
+		isFizz := numberDivisibleBy(i, fizzAt)
+		isBuzz := numberDivisibleBy(i, buzzAt)
+
+		if !isFizz && !isBuzz {
 			result[i-1] = strconv.FormatInt(i, 10)
 			continue
 		}
 
-		if i%fizzAt == 0 {
-			result[i-1] = "Fizz"
+		if isFizz {
+			result[i-1] = Fizz
 		}
 
-		if i%buzzAt == 0 {
-			result[i-1] += "Buzz"
+		if isBuzz {
+			result[i-1] += Buzz
 		}
 	}
 
 	return result
+}
+
+// numberDivisibleBy returns true if the number i is divisible by the provided divisor
+// If divisor is 0 this function returns false
+func numberDivisibleBy(i, divisor int64) bool {
+	if divisor == 0 {
+		return false
+	}
+	return i%divisor == 0
 }
--- unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
+++ unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
@@ -1,7 +1,21 @@
 package fizzbuzz
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestFizzBuzz(t *testing.T) {
-	t.Skip("TODO: Add tests")
+func TestFizzBuzz_EdgeCases(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
+		result := FizzBuzz(0, 3, 5)
+		assert.Len(t, result, 0)
+	})
+	t.Run("Zero Fizz", func(t *testing.T) {
+		result := FizzBuzz(20, 0, 5)
+		assert.Len(t, result, 20)
+	})
+	t.Run("Zero Buzz", func(t *testing.T) {
+		result := FizzBuzz(20, 3, 0)
+		assert.Len(t, result, 20)
+	})
 }
