68ce995a82d62f1b6c31d4f97ef0cf145feadd66
Andrew Querol <andrew@querol.me>
2022-06-25 15:00:21 -0500

    First pass on moving to a separate package to allow for unit testing

---
+++ input-processing/filter/filter.go
@@ -0,0 +1,96 @@
+package filter
+
+import (
+	"bytes"
+	"errors"
+	"io"
+)
+
+// Filter A helper that allows filtering text lines from a source to a destination if it contains the filter term
+// This class is not thread safe, only one thread may access filter at a time.
+type Filter struct {
+	filterTerm            string
+	possibleMatchPosition int
+	matchedLine           bool
+	buffer                []byte
+	inProgressLine        bytes.Buffer
+}
+
+func New(filterTerm string, blockSize uint64) *Filter {
+	return &Filter{
+		filterTerm:            filterTerm,
+		possibleMatchPosition: 0,
+		matchedLine:           false,
+		buffer:                make([]byte, blockSize),
+	}
+}
+
+// Process Reads from source and if a line matches the filterTerm writes to dest. Will block until there is an error or EOF is hit
+func (f *Filter) Process(dest io.Writer, source io.Reader) error {
+	// Make sure we reset for possible future runs
+	defer f.Reset()
+
+	for {
+		sizeRead, err := f.readBuffer(source)
+		for _, b := range f.buffer[:sizeRead] {
+			if err := f.inProgressLine.WriteByte(b); err != nil {
+				// TODO: Wrap error
+				return err
+			}
+
+			if b == '\n' {
+				if f.matchedLine {
+					if _, err := f.inProgressLine.WriteTo(dest); err != nil {
+						// TODO Wrap error
+						return err
+					}
+				}
+				f.Reset()
+				continue
+			}
+
+			if !f.matchedLine {
+				// Check if the byte matches the next possible character in the filter term if we haven't fully matched yet.
+				if f.possibleMatchPosition < len(f.filterTerm) && b == f.filterTerm[f.possibleMatchPosition] {
+					// We are matching the filter term still, increment the counter
+					f.possibleMatchPosition++
+					if f.possibleMatchPosition == len(f.filterTerm) {
+						f.matchedLine = true
+					}
+				} else if f.possibleMatchPosition > 0 {
+					// Reset the possible match counter, byte did not match
+					f.possibleMatchPosition = 0
+				}
+			}
+		}
+
+		// TODO Wrap error
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			return err
+		}
+	}
+}
+
+func (f *Filter) Reset() {
+	f.inProgressLine.Reset()
+	f.possibleMatchPosition = 0
+	f.matchedLine = false
+}
+
+func (f *Filter) readBuffer(reader io.Reader) (int, error) {
+	sizeRead := 0
+	for {
+		n, err := reader.Read(f.buffer[sizeRead:])
+		sizeRead += n
+		if err != nil {
+			return sizeRead, err
+		}
+		if sizeRead == len(f.buffer) {
+			break
+		}
+	}
+	return sizeRead, nil
+}
--- input-processing/main.go
+++ input-processing/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
+	"github.com/stackpath/backend-developer-tests/input-processing/filter"
 	"io"
 	"log"
 	"os"
@@ -28,64 +28,10 @@ func main() {
 		return
 	}
 
-	buffer := make([]byte, blockSize)
-	possibleMatchPosition := 0
-	matchedLine := false
-	var inProgressLine bytes.Buffer
-
-	for {
-		sizeRead, err := readBuffer(os.Stdin, buffer)
-		for _, b := range buffer[:sizeRead] {
-			if err := inProgressLine.WriteByte(b); err != nil {
-				log.Fatalln("Error storing in progress line.", err)
-			}
-
-			if b == '\n' {
-				if matchedLine {
-					if _, err := inProgressLine.WriteTo(os.Stdout); err != nil {
-						log.Fatalln("Error writing matching line to standard out.", err.Error())
-					}
-				} else {
-					inProgressLine.Reset()
-					possibleMatchPosition = 0
-					matchedLine = false
-				}
-			}
-
-			if !matchedLine {
-				// Check if the byte matches the next possible character in the filter term if we haven't fully matched yet.
-				if possibleMatchPosition < len(filterTerm) && b == filterTerm[possibleMatchPosition] {
-					// We are matching the filter term still, increment the counter
-					possibleMatchPosition++
-					if possibleMatchPosition == len(filterTerm) {
-						matchedLine = true
-					}
-				} else if possibleMatchPosition > 0 {
-					// Reset the possible match counter, byte did not match
-					possibleMatchPosition = 0
-				}
-			}
-		}
-
-		if err != nil {
-			break
-		}
+	textFilter := filter.New(filterTerm, blockSize)
+	if err := textFilter.Process(os.Stdout, os.Stdin); err != nil {
+		log.Fatalln("Error encountered while filtering", err)
 	}
-}
-
-func readBuffer(reader io.Reader, buffer []byte) (int, error) {
-	sizeRead := 0
-	for {
-		n, err := reader.Read(buffer[sizeRead:])
-		sizeRead += n
-		if err != nil {
-			return sizeRead, err
-		}
-		if sizeRead == len(buffer) {
-			break
-		}
-	}
-	return sizeRead, nil
 }
 
 func init() {
